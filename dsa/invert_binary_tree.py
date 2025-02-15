c1496a07bae34d0fde56f1db8fbc654e5e6d9184
Vincent Joy <vincentjoyhere@gmail.com>
2025-02-15 11:13:57 +0530

    invert binary tree dsa question (lvel: easy)

---
+++ dsa/invert_binary_tree.py
@@ -0,0 +1,37 @@
+# Given the root of a binary tree, invert the tree, and return its root.
+
+# Example 1:
+# Input: root = [4,2,7,1,3,6,9]
+# Output: [4,7,2,9,6,3,1]
+
+# Example 2:
+# Input: root = [1,2,3,4,5]
+# Output: [5,4,3,2,1]
+
+# Example 3:
+# Input: root = [2,1,3]
+# Output: [2,3,1]
+
+# Example 4:
+# Input: root = []
+# Output: []
+
+class TreeNode:
+    def _init_(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        # Base case: If root is None, return None
+        if not root:
+            return None
+
+        root.left, root.right = root.right, root.left
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
